1947dd5cb72096e1797a20216d6c94cf245cb94e
rafael <rafael.rodrigues.bento@outlook.com>
2023-02-07 23:43:24 -0300

    Image component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,26 @@
+import { globalStyles } from '../styles/global'
 import type { AppProps } from 'next/app'
 
+globalStyles()
+
+import Image from 'next/image'
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image 
+          src={logoImg.src}
+          width={130}
+          height={52} 
+          alt="" 
+        />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }

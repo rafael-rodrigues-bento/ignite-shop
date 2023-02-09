54dd1a0b7ddb6a0841273bf7ebe583611c3e24ea
rafael <rafael.rodrigues.bento@outlook.com>
2023-02-08 23:46:16 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,38 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "./home"
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px'
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image 
+          src={camiseta1}
+          alt=""
+          width={520}
+          height={480}
+        />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image 
+          src={camiseta3}
+          alt=""
+          width={520}
+          height={480}
+        />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }

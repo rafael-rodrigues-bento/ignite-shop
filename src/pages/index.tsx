4fdec55f279d85c536a09ddf9316ea4176ded71c
rafael <rafael.rodrigues.bento@outlook.com>
2023-02-13 22:51:41 -0300

    Integração com o stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,11 @@ import Image from "next/image"
 
 import { useKeenSlider } from 'keen-slider/react'
 
-import { HomeContainer, Product } from "./home"
-
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
+import { HomeContainer, Product } from "../styles/pages/home"
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 interface HomeProps {
@@ -39,7 +35,7 @@ export default function Home({ products }: HomeProps) {
           return (
             <Product key={product.id} className="keen-slider__slide">
               <Image 
-                src={camiseta1}
+                src={product.imageUrl}
                 alt=""
                 width={520}
                 height={480}
@@ -47,7 +43,7 @@ export default function Home({ products }: HomeProps) {
 
         <footer>
           <strong>{product.name}</strong>
-          <span>{product.imageUrl}</span>
+          <span>{product.price}</span>
         </footer>
       </Product>
           )
@@ -57,7 +53,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
@@ -70,13 +66,17 @@ export const getServerProps: GetServerSideProps = async () => {
     id: product.id,
     name: product.name,
     imageUrl: product.images[0],
-    price: price.unit_amount! / 100,
+    price: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price.unit_amount! / 100)
     }
   })
 
   return {
     props : {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,4 +1,4 @@
-import { styled } from "../styles";
+import { styled } from "..";
 
 export const HomeContainer = styled('main', {
   display: 'flex',
